82c64991d7d4d259b9b629dddc86d5ea5e45bfd9
Magnus Höjer <107888656+magnushjr@users.noreply.github.com>
2022-06-29 00:48:14 +0200

    Fix

--- todo/src/TodoList.js
+++ todo/src/TodoList.js
@@ -2,34 +2,34 @@ import React from 'react'
 import InputForm from './InputForm'
 
 class TodoList extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { items: [] };
-	}
-
-	addItem = (item) => {
-		this.setState(prevState => ({
-			items: prevState.items.concat([item])
-		}));
-	}
+   constructor(props) {
+      super(props);
+      this.state = { items: [] };
+   }
+   
+   addItem = (item) => {
+      this.setState(prevState => ({
+         items: prevState.items.concat([item])
+      }));
+   }
 	
-	render() {
-		const countMsg = this.state.items.length === 1
-			? <p>You have {this.state.items.length} thing to do.</p>
-			: <p>You have {this.state.items.length} things to do.</p>;
+   render() {
+      const countMsg = this.state.items.length === 1
+         ? <p>You have {this.state.items.length} thing to do.</p>
+         : <p>You have {this.state.items.length} things to do.</p>;
 		
-		return (
-			<div>
-			{countMsg}
-				<ul>
-					{this.state.items.map((item) =>
-						<li>{item}</li>
-					)}
-					</ul>
-					<InputForm prompt="Add" onSubmit={this.addItem} />
-				</div>
-		);
-	}
+      return (
+         <div>
+            {countMsg}
+            <ul>
+               {this.state.items.map((item) =>
+                  <li>{item}</li>
+               )}
+            </ul>
+            <InputForm prompt="Add" onSubmit={this.addItem} />
+         </div>
+      );
+   }
 }
 
 export default TodoList;
