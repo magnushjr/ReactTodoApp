8ca89f2f8de84a2a7fb614458a700023da14141c
Magnus Höjer <107888656+magnushjr@users.noreply.github.com>
2022-06-29 00:33:33 +0200

    Basic functionality in place

---
+++ todo/src/InputForm.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class InputForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { value: "" };
+	}
+	
+	handleChange = (e) => {
+		this.setState({ value: e.target.value });
+	}
+	
+	onSubmit = (e) => {
+		e.preventDefault();
+		this.props.onSubmit(this.state.value);
+		this.setState({ value: "" });
+	}
+	
+	render() {
+		return (
+			<form>
+				<input type="text" value={this.state.value} onChange={this.handleChange} />
+				<button onClick={this.onSubmit}>{this.props.prompt}</button>
+			</form>
+		)
+	}
+}
+
+export default InputForm;
